d53bcc4db5f843c2a009de88de7c655be225185a
triple1566 <ljhub1566@gmail.com>
2024-10-05 01:28:00 -0400

    cleared some files

---
+++ instantbay/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const App = () => {
+  return (
+    <view>
+        <text>Hello World</text>
+    </view>
+  )
+}
+
+export default App
