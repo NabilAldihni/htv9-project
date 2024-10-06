a2611fd2f8cf3a51b0d52c9894ac338ac84a1a64
triple1566 <ljhub1566@gmail.com>
2024-10-05 22:47:02 -0400

    working commit

--- instantbay/components/App.jsx
+++ instantbay/components/App.jsx
@@ -4,7 +4,6 @@ import { StyleSheet } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import ItemScrollView from './scrollview/ItemScrollView';
-import CameraView from './cameraview/CameraView';
 import LoginView from './loginview/LoginView';
 
 
@@ -16,7 +15,6 @@ const App = () => {
 
       <ScreenStack.Navigator>
         <ScreenStack.Screen name="Login" component={LoginView} options={{headerShown: false}} />
-        <ScreenStack.Screen name="Camera" component={CameraView} options={{headerShown: false}} />
         <ScreenStack.Screen name="Scroll" component={ItemScrollView} options={{headerShown: false}} />
       </ScreenStack.Navigator>
     </NavigationContainer>
