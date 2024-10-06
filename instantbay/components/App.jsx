ef79ad1e1fed051da01adc0305e3e42a5e52c5e1
Armaan204 <armaanrehmanshah1@gmail.com>
2024-10-05 22:15:13 -0400

    Pushing white screen issue

--- instantbay/components/App.jsx
+++ instantbay/components/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ItemScrollView from './scrollview/ItemScrollView';
+import LoginView from './loginview/LoginView';
+import CameraView from './cameraview/CameraView';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <ItemScrollView />
-    </View>
+    <NavigationContainer independent={true}>
+      <Stack.Navigator>
+       <Stack.Screen name="LoginView" component={LoginView} />
+       <Stack.Screen name="CameraView" component={CameraView} />
+       <Stack.Screen name="ItemScrollView" component={ItemScrollView} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
--- instantbay/components/loginview/LoginView.jsx
+++ instantbay/components/loginview/LoginView.jsx
@@ -1,37 +1,36 @@
 import React from 'react';
+import { TouchableOpacity, Text, View, StyleSheet } from 'react-native';
 
-const LoginView = () => {
-  const buttonStyle = {
-    padding: '10px 20px',
-    fontSize: '16px',
-    color: '#fff',
-    backgroundColor: '#007BFF',
-    border: 'none',
-    borderRadius: '5px',
-    cursor: 'pointer',
-    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-    transition: 'background-color 0.3s ease',
-  };
-
-  const handleMouseOver = (e) => {
-    e.target.style.backgroundColor = '#0056b3';
-  };
-
-  const handleMouseOut = (e) => {
-    e.target.style.backgroundColor = '#007BFF';
-  };
-
+const LoginView = ({ navigation }) => {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <button
-        style={buttonStyle}
-        onMouseOver={handleMouseOver}
-        onMouseOut={handleMouseOut}
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('CameraView')} // Navigate using React Navigation
       >
-        Go to Items
-      </button>
-    </div>
+        <Text style={styles.buttonText}>Go to Camera</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    padding: 10,
+    backgroundColor: '#007BFF',
+    borderRadius: 5,
+    elevation: 3,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: '#fff',
+    textAlign: 'center',
+  },
+});
 
 export default LoginView;
