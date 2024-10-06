af8a8731c61501cf2036ed7bb86893974503e721
triple1566 <ljhub1566@gmail.com>
2024-10-05 22:36:56 -0400

    added camera to router

--- instantbay/components/App.jsx
+++ instantbay/components/App.jsx
@@ -3,6 +3,7 @@ import { StyleSheet } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import ItemScrollView from './scrollview/ItemScrollView';
+import CameraView from './cameraview/CameraView';
 import LoginView from './loginview/LoginView';
 
 
@@ -13,6 +14,7 @@ const App = () => {
     <NavigationContainer independent={true}>
       <ScreenStack.Navigator>
         <ScreenStack.Screen name="Login" component={LoginView} options={{headerShown: false}} />
+        <ScreenStack.Screen name="Camera" component={CameraView} options={{headerShown: false}} />
         <ScreenStack.Screen name="Scroll" component={ItemScrollView} options={{headerShown: false}} />
       </ScreenStack.Navigator>
     </NavigationContainer>
