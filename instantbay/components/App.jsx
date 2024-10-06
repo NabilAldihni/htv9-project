93d3e3be96c29f2a76193787213fe2b836d7afe3
triple1566 <ljhub1566@gmail.com>
2024-10-06 00:40:00 -0400

    login input layout

--- instantbay/components/App.jsx
+++ instantbay/components/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
 
 const styles = StyleSheet.create({
   container:{
-    flex: 1,
+    flex: 1
   }
 })
 
--- instantbay/components/loginview/LoginView.jsx
+++ instantbay/components/loginview/LoginView.jsx
@@ -1,24 +1,65 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 import { TouchableOpacity } from 'react-native';
 
 const LoginView = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome to InstantBay!</Text>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate('Camera')}
-      >
-        <Text style={styles.buttonText}>Start Scanning</Text>
-      </TouchableOpacity>
+    <View style={{flex: 1}}>
+      <View style={styles.upperContainer}>
+        <Text style={styles.title}>Welcome to InstantBay!</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('Camera')}
+        >
+          <Text style={styles.buttonText}>Start Scanning</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.lowerContainer}>
+
+        <View style={{padding:7, width: '60%', shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 3.84,}}>
+          <View style={styles.textinput}>
+            <TextInput placeholder='Username'/>
+          </View>
+        </View>
+        <View style={{padding:7, width: '60%', shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 3.84,}}>
+          <View style={styles.textinput}>
+            <TextInput placeholder='Password'/>
+          </View>
+        </View>
+        <View style={{padding:7}}>
+          <TouchableOpacity style={styles.loginbutton}>
+            <Text style={styles.loginbuttonText}>Log In</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  upperContainer: {
+    flex: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  lowerContainer:{
+    backgroundColor: '#CED4DA',
+    borderTopRightRadius: 40,
+    borderTopLeftRadius: 40,
+    flex: 2,
+    flexDirection:'col',
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -28,8 +69,30 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     elevation: 3,
   },
+  loginbutton: {
+    padding: 15,
+    backgroundColor: '#4361EE',
+    fontSize: '30',
+    borderRadius: 10,
+    elevation: 3,
+    // Add shadow properties
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
   buttonText: {
     fontSize: 16,
+    color: '#fff',
+    fontWeight: '500',
+    textAlign: 'center',
+  },
+  loginbuttonText: {
+    fontSize: 20,
+    fontWeight: '800',
     color: '#fff',
     textAlign: 'center',
   },
@@ -38,6 +101,23 @@ const styles = StyleSheet.create({
     color: '#1D1D1F',
     marginBottom: 20,
   },
+  textinput:{
+    fontWeight: '600',
+    backgroundColor:'#F5F5F7',
+    padding: 5,
+    borderRadius: 10,
+    width: '100%',
+    flexDirection:'row'
+  },
+  backgroundVideo: {
+    height: '100%',
+    position: "absolute",
+    top: 0,
+    left: 0,
+    alignItems: "stretch",
+    bottom: 0,
+    right: 0
+  }
 });
 
 export default LoginView;
